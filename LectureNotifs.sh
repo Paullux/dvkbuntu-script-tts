a65d4dea328f2a900348edcf928e8789d80d45b7
Paul WOISARD <paulwoisard@gmail.com>
2019-10-02 08:02:16 +0200

    Mise à jour LectureNotis.sh

--- LectureNotifs.sh
+++ LectureNotifs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dbus-monitor "interface='org.freedesktop.Notifications'" | grep --line-buffered "string" | grep --line-buffered -e method -e ":" -e '""' -e urgency -e notify -v | grep --line-buffered '.*(?=string)|(?<=string).*' -oPi | grep --line-buffered -v '^\s*$' | grep --line-buffered -v "image-data\|default\|View\|desktop-entry\|variant" | xargs -I '{}' google_speech -l fr-fr {}
+dbus-monitor "interface='org.freedesktop.Notifications'" | grep --line-buffered "string" | grep --line-buffered -e method -e ":" -e '""' -e urgency -e notify -v | grep --line-buffered '.*(?=string)|(?<=string).*' -oPi | grep --line-buffered -v '^\s*$' | grep --line-buffered -v "image-data\|default\|View\|desktop-entry\|variant\|not_a_real_icon\|org.kde\|transient\|x-kde\|kdeconnect\|notification\|Répondre" | xargs -I '{}' google_speech -l fr-fr {}
