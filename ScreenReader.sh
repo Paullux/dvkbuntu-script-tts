47aeb0a62c60805860ad899aa86135e4e1843d97
Paul WOISARD <paulwoisard@gmail.com>
2019-10-04 19:02:39 +0200

    remise en ordre du script

--- ScreenReader.sh
+++ ScreenReader.sh
@@ -6,6 +6,11 @@
 workDir="/tmp/OCR"
 [ ! -d "$workDir" ] && mkdir "$workDir"
 
+# Initialisation des variables
+unset MOUSE
+unset CURRENT_WINDOW
+unset SELECTION
+
 #Options choice
 case $1 in
     souris)
@@ -18,49 +23,49 @@ case $1 in
         SELECTION=true;;
 
     *) 
+        #Quit if non choice
         a="Vous devez préciser une option"
         google_speech -l fr-fr  "$a" 
         exit 0;;
 esac
 
 #Make screenshots
-
 if $MOUSE; then
-#Rectangle notation :
-# (0,1); (1,1)
-# (0,0); (1,0)
+    #Rectangle notation :
+    # (0,1); (1,1)
+    # (0,0); (1,0)
 
-#Offset the screenshot rectangle to the upper-left corner (0,1) by this amount in the X/Y direction
-#Might be a problem for i18n & RTL languages but... Oh, well
-NegX01Offset=300
-NegY01Offset=75
-#Size of the rectangle
-posX10=600
-posY10=150
-#Set X,Y to the Cursor position.
-eval $(xdotool getmouselocation --shell)
-posX01=$(($X-$NegX01Offset))
-posY01=$(($Y-$NegY01Offset))
-#ScreenShot
-scrot -a "$posX01","$posY01","$posX10","$posY10" -o "$workDir"/OCR.png;;
+    #Offset the screenshot rectangle to the upper-left corner (0,1) by this amount in the X/Y direction
+    #Might be a problem for i18n & RTL languages but... Oh, well
+    NegX01Offset=300
+    NegY01Offset=75
+    #Size of the rectangle
+    posX10=600
+    posY10=150
+    #Set X,Y to the Cursor position.
+    eval $(xdotool getmouselocation --shell)
+    posX01=$(($X-$NegX01Offset))
+    posY01=$(($Y-$NegY01Offset))
+    #ScreenShot
+    scrot -a "$posX01","$posY01","$posX10","$posY10" -o "$workDir"/OCR.png;;
 fi
 
 if $CURRENT_WINDOW; then
-# To prepare the commande
-a="La fenêtre courante est"
-b=$(cat /proc/"$(xdotool getwindowpid $(xdotool getactivewindow))"/comm)
-c="La fenêtre courante contient"
-d="${a} ${b} ${c}"
-google_speech -l fr-fr "$d"
-#ScreenShot
-scrot -u -o "$workDir"/OCR.png
+    # To prepare the commande
+    a="La fenêtre courante est"
+    b=$(cat /proc/"$(xdotool getwindowpid $(xdotool getactivewindow))"/comm)
+    c="La fenêtre courante contient"
+    d="${a} ${b} ${c}"
+    google_speech -l fr-fr "$d"
+    #ScreenShot
+    scrot -u -o "$workDir"/OCR.png
 fi
 
 if $SELECTION; then
-#To prepare the commande
-google_speech -l fr-fr  "sélectionnez la zone à lire"
-#ScreenShot
-import "$workDir"/OCR.png;;
+    #To prepare the commande
+    google_speech -l fr-fr  "sélectionnez la zone à lire"
+    #ScreenShot
+    import "$workDir"/OCR.png;;
 fi
 
 
